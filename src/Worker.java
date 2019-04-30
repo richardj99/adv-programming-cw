91fe773723bcd652bba79e3c2dc2e20f07de4184
Richard Jones <richardj99@sky.com>
2019-04-30 23:27:06 +0100

    Revert "Update Worker.java"
    This reverts commit 54a7a6dc5dd2298b3e439c8f6406e60bfad0cb85.

--- src/Worker.java
+++ src/Worker.java
@@ -54,7 +54,11 @@ public class Worker implements Runnable {
     		job = jobStack.pop();
     		Integer[] resourcesUsed = new Integer[job.getResourceRequirement()];
     		while(resources == null) {
+    			try{
     				resources = resourceStack.pop(job.getResourceRequirement());
+    			} catch(NullPointerException e) {
+    				;
+    			}
     		}
     		try {
     			Thread.sleep(job.getTimeToComplete());
